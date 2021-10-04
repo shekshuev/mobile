cc3e5f523618f770d484543d80d00c14ea851e01
Sparrow <KachKinalap@ya.ru>
2021-10-04 17:23:02 +0300

    gh

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to star working on your app!</Text>
+      <Text>Up App.js to star working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
