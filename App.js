f7a07a2d7989d2add43689228de4d659aa777ec7
Sparrow <KachKinalap@ya.ru>
2021-10-05 18:39:48 +0300

    fg

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-import { Navbar } from './src/Navbar';
+import { Navbar } from './src/Navbar.js';
 
 
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
-export const Navbar = (props) => {
+export default Navbar = (props) => {
     return (
         <View style={styles.navbar}>
             <Text style={styles.text}>Todo App</Text>
