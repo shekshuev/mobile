75018f5f487c7da4f7380c257959cb9b95d116b0
Sparrow <KachKinalap@ya.ru>
2021-10-06 00:44:21 +0300

    авп

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-import { Navbar } from './Navbar';
+import {Navbar} from './src/Navbar';
 
 export default function App() {
   return (
-    <View style={StyleSheet.container}>
-      <Navbar></Navbar>
+    <View>
+      <Navbar title="Авторизация"></Navbar>
     </View>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,36 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {SafeAreaView, Text, StyleSheet, Image} from 'react-native'
 
-export default function Navbar() {
+export const Navbar = props => {
     return (
-        <View style={styles.navbar}>
-            <Text style={styles.text}>Todo App</Text>
-        </View>
+<SafeAreaView style={styles.navbar}>
+    <Image style={styles.exit} source={require('./img/exit.png')}/>
+    <Text style={styles.text}>{props.title}</Text>
+    <Image style={styles.menu} source={require('./img/menu.png')}/>
+</SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     navbar: {
-        height: '70',
+        paddingTop:30,
+        height: 90,
+        flexDirection:'row',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: 'lightblue'
     },
     text: {
         
+    },
+    exit: {
+        width:30,
+        height:30,
+        resizeMode: 'stretch',
+    },
+    menu: {
+        width:50,
+        height:50,
+        resizeMode: 'stretch',
     }
 })
